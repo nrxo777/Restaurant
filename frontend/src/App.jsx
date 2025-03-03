d4fd6ad2e72aafe2e7f010251311090e0267ea4d
nrxo777 <noorxcalma@gmail.com>
2025-03-03 23:26:35 +0600

    Component Tree

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,12 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import MainComponents from "./components/MainComponents";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1>Hello JI</h1>
-    </>
+    <div className="App">
+      <MainComponents />
+    </div>
   );
 }
 
