115b1789fea33f479efebba70daf0682b9820b55
nrxo777 <noorxcalma@gmail.com>
2025-03-04 21:58:47 +0600

    fixing_redirect_issue

--- frontend/src/components/body/Body.jsx
+++ frontend/src/components/body/Body.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Navigate, Routes } from "react-router-dom";
 import Home from "./Home";
 import Menu from "./Menu.jsx";
 import About from "./About";
@@ -9,10 +9,11 @@ const Body = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/menu" element={<Menu />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/" element={<Navigate to="/home" />} />
       </Routes>
     </div>
   );
