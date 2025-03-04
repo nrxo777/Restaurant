e475faddca8135f4ceb20f17833c685ff7411d8f
nrxo777 <noorxcalma@gmail.com>
2025-03-04 23:28:26 +0600

    footer_component

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-auto">
+            <p>Epic Bites &#169; Copyright 2025</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
