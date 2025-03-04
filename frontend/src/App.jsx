c6b4505fc2bcb3ab9ffe98c5a17e8fd2d81e7ba4
nrxo777 <noorxcalma@gmail.com>
2025-03-04 17:11:49 +0600

    designing_navigation_bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import MainComponents from "./components/MainComponents";
 
--- frontend/src/components/header/Navigation.jsx
+++ frontend/src/components/header/Navigation.jsx
@@ -1,11 +1,40 @@
-import React from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
+import React, { useState } from "react";
+import {
+  Nav,
+  Navbar,
+  NavItem,
+  NavbarBrand,
+  NavbarToggler,
+  NavLink,
+  Collapse,
+} from "reactstrap";
 
 const Navigation = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const navToggle = () => {
+    setIsNavOpen(!isNavOpen);
+  };
   return (
     <>
-      <Navbar color="dark" dark>
-        <NavbarBrand href="/">Restaurant</NavbarBrand>
+      <Navbar color="dark" dark expand="sm">
+        <NavbarToggler onClick={navToggle} />
+        <NavbarBrand href="/">Epic Bites</NavbarBrand>
+        <Collapse isOpen={isNavOpen} navbar>
+          <Nav className="me-auto" navbar>
+            <NavItem>
+              <NavLink to="/">Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="/menu">Menu</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="/about">About</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="/contact">Contact</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
       </Navbar>
     </>
   );
