a6e5883d8b6672a0bd82d0273fd72633181a6d33
nrxo777 <noorxcalma@gmail.com>
2025-03-04 22:32:50 +0600

    contact_form

--- frontend/src/components/body/Contact.jsx
+++ frontend/src/components/body/Contact.jsx
@@ -1,8 +1,130 @@
 import React, { Component } from "react";
+import { Button, Form, FormGroup, Label, Input, Col } from "reactstrap";
 
 export class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstname: "",
+      lastname: "",
+      telnum: "",
+      email: "",
+      agree: false,
+      contactType: "Tel.",
+      message: "",
+    };
+  }
   render() {
-    return <div>Contact</div>;
+    return (
+      <div className="container">
+        <div className="row row-content">
+          <div className="col-12">
+            <h3>Send us your Feedback</h3>
+            <div className="col-12">
+              <Form>
+                <FormGroup row>
+                  <Label htmlFor="firstname" md={2}>
+                    First Name
+                  </Label>
+                  <Col md={10}>
+                    <Input
+                      type="text"
+                      name="firstname"
+                      placeholder="First Name"
+                      value={this.state.firstname}
+                    />
+                  </Col>
+                </FormGroup>
+                <FormGroup row>
+                  <Label htmlFor="lastname" md={2}>
+                    Last Name
+                  </Label>
+                  <Col md={10}>
+                    <Input
+                      type="text"
+                      name="lastname"
+                      placeholder="Last Name"
+                      value={this.state.lastname}
+                    />
+                  </Col>
+                </FormGroup>
+                <FormGroup row>
+                  <Label htmlFor="telnum" md={2}>
+                    Contact Tel.
+                  </Label>
+                  <Col md={10}>
+                    <Input
+                      type="tel"
+                      name="telnum"
+                      placeholder="Tel Number"
+                      value={this.state.telnum}
+                    />
+                  </Col>
+                </FormGroup>
+                <FormGroup row>
+                  <Label htmlFor="email" md={2}>
+                    Email ID
+                  </Label>
+                  <Col md={10}>
+                    <Input
+                      type="email"
+                      name="email"
+                      placeholder="Email ID"
+                      value={this.state.email}
+                    />
+                  </Col>
+                </FormGroup>
+                <FormGroup row>
+                  <Col md={{ size: 6, offset: 2 }}>
+                    <FormGroup check>
+                      <Label check>
+                        <input
+                          type="checkbox"
+                          name="agree"
+                          checked={this.state.agree}
+                        />{" "}
+                        <strong>May we contact you</strong>
+                      </Label>
+                    </FormGroup>
+                  </Col>
+                  <Col md={{ size: 3, offset: 1 }}>
+                    <Input
+                      type="select"
+                      name="contactType"
+                      value={this.state.contactType}
+                    >
+                      <option>Telephone</option>
+                      <option>Email</option>
+                    </Input>
+                  </Col>
+                </FormGroup>
+                <FormGroup row>
+                  <Label htmlFor="message" md={2}>
+                    Your Feedback
+                  </Label>
+                  <Col md={10}>
+                    <Input
+                      type="textarea"
+                      name="message"
+                      // placeholder="Your Feedback"
+                      value={this.state.message}
+                      rows="12"
+                    />
+                  </Col>
+                </FormGroup>
+                <FormGroup>
+                  <Col md={{ size: 10, offset: 2 }}>
+                    <Button type="submit" color="btn btn-secondary">
+                      Send Feedback
+                    </Button>
+                  </Col>
+                </FormGroup>
+              </Form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
